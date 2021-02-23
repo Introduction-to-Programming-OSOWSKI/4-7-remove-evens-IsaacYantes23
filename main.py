c0bd3b832039ebe4fcda5048c0c46d1a746c012e
IsaacYantes23 <817296@mystma.org>
2021-02-23 16:13:27 +0000

    removeevenstry 2

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def removeEvens(k):
     newList = k
     numPop = 0
     for i in range(0,len(k)):
-        if k[i -numPop] % 2 == 0:
+        if k[i -numPop] % 2 == 0 and newList[i - numPop] != 0:
             newList.pop(i -  numPop)
             numPop = numPop + 1
             
